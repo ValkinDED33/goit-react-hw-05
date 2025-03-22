f4fda131c6a79d71a15dfe94371cb1c61a37d1d1
ValkinDED <sonyerik289@gmail.com>
2025-03-22 17:28:40 +0100

    goit-react-hw-05

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Измени, если деплоишь в подкаталог (например, "/my-app/")
+  define: {
+    "process.env": {},
+  },
   build: {
     sourcemap: true,
-  },
-  server: {
-    host: true,
-    port: 5173,
-    open: true,
-    strictPort: true, // Если порт занят, Vite не переключится на другой
-    middlewareMode: false, // 🔄 Убедимся, что Vite правильно обрабатывает маршруты
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  preview: {
-    port: 4173,
-    strictPort: true,
   },
 });
