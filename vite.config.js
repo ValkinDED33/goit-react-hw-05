513849d153193eff151db510fe99b515243b4461
ValkinDED <sonyerik289@gmail.com>
2025-03-22 18:55:22 +0100

    goit-react-hw-05

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
   define: {
     "process.env": {},
   },
   build: {
-    outDir: "dist",
     sourcemap: true,
   },
 });
